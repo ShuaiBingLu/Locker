c2af693861f3c0366888236095253bad1234fc80
Lu Bingshuai <bingshuai.lu@thoughtworks.com>
2020-09-08 20:54:22 +0800

    should throw exception given full locker and no robot when deposit bag

--- src/test/java/cn/xpbootcamp/locker/LockerRobotManagerTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerRobotManagerTest.java
@@ -1,11 +1,13 @@
 package cn.xpbootcamp.locker;
 
 import cn.xpbootcamp.domain.Bag;
+import cn.xpbootcamp.exception.DepositBagFailedException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LockerRobotManagerTest {
 
@@ -15,5 +17,13 @@ public class LockerRobotManagerTest {
         List<Locker> lockers = List.of(new Locker(3L), new Locker(4L));
         lockerRobotManager.setLocker(lockers);
         assertThat(lockerRobotManager.deposit(new Bag())).isNotNull();
+    }
+
+    @Test
+    void should_return_deposit_failed_given_two_full_locker_and_0_robot_when_deposit_bag() {
+        LockerRobotManager lockerRobotManager = new LockerRobotManager();
+        List<Locker> lockers = List.of(new Locker(0L), new Locker(0L));
+        lockerRobotManager.setLocker(lockers);
+        assertThatThrownBy(() -> lockerRobotManager.deposit(new Bag())).isInstanceOf(DepositBagFailedException.class);
     }
 }
